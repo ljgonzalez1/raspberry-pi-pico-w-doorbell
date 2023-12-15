a3f276f9068b271298e30fa35b8dc29b68f13637
Luis González <ljgonzalez@uc.cl>
2023-12-15 05:38:39 -0300

    ♻️ refactor(main.py): Simplify pin handler and optimize main loop.

--- main.py
+++ main.py
@@ -11,17 +11,13 @@ from logging import dprint as print, show_mem as mem_info
 
 onboard_led = Pin(settings.LED_PIN, mode=Pin.OUT, value=0)
 doorbell_pin = Pin(settings.DOORBELL_PIN, Pin.IN, Pin.PULL_UP)
-
+messages = Messages()
 heart = Heart(onboard_led)
 
 
 def pin_handler(pin):
-    heart.tachycardia()
     print(f"Interrupt detected in pin pin {pin}")
-
     sleep(0.1)
-
-    messages = Messages()
     messages.send()
 
 
@@ -31,10 +27,9 @@ def read_int(pin):
         pin_handler(pin)
 
 
-
 if __name__ == "__main__":
     while True:
         for _ in heart.beat():
-            read_int(pin)
+            read_int(doorbell_pin)
             sleep(0.001)
 
