82b00fe16f1619d7ea3f18e3b9e2d5d9d342f503
Luis González <ljgonzalez@uc.cl>
2023-12-18 15:26:52 -0300

    📝 docs(src/main.py): Add comments and function descriptions

--- src/main.py
+++ src/main.py
@@ -15,15 +15,43 @@ heart = Heart(onboard_led)
 
 
 def debounce(pin):
+    """
+    A function that sets an interrupt on a pin and waits for a specified amount
+    of time before triggering the interrupt handler.
+
+    Args:
+        pin (Pin): The pin to set the interrupt on.
+
+    Returns:
+        None.
+
+    """
     pin.irq(handler=None)
 
-    interrupt_handler(pin)
+    # Calls the interrupt_handler function when an interrupt is detected on the
+    # pin.
+    pin.irq(trigger=Pin.IRQ_FALLING, handler=interrupt_handler)
+
+    # Waits for a specified amount of time before triggering the interrupt
+    # handler.
     sleep(0.2)
 
+    # Resets the interrupt on the pin and sets it to trigger on a low-level
+    # signal.
     pin.irq(trigger=Pin.IRQ_LOW_LEVEL, handler=debounce)
 
 
 def interrupt_handler(pin):
+    """
+    This function is called when an interrupt is detected on the specified pin.
+
+    Args:
+        pin (Pin): The pin that generated the interrupt.
+
+    Returns:
+        None.
+
+    """
     print(f"Interrupt detected in pin: {pin}")
     heart.off()
     messages = Messages()
